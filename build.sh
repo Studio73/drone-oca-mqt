52efeb223724dc27d341a3b7ced01013463802af
Pablo Fuentes <pablo@studio73.es>
2019-06-17 20:20:37 +0200

    [REM] Not consumed args

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-             --build-arg VCS_REF=`git rev-parse --short HEAD` \
-             -f $1/Dockerfile . -t drone-oca-mqt:$1
+docker build -f $1/Dockerfile . -t drone-oca-mqt:$1
